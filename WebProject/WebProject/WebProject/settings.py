d16e76bc7ab2cc12325765e0b770d917097f1a5f
Exvir <47493621+Exvir@users.noreply.github.com>
2019-07-21 16:12:57 +0300

    Update settings.py

--- WebProject/WebProject/WebProject/settings.py
+++ WebProject/WebProject/WebProject/settings.py
@@ -59,8 +59,8 @@ REGISTRATION_AUTO_LOGIN = True # Automatically log the user in.
 #Пока не менял
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'enotukit@gmail.com'
-EMAIL_HOST_PASSWORD = 'kiticq348348'
+EMAIL_HOST_USER = 'myemail@gmail.com'
+EMAIL_HOST_PASSWORD = 'mypassword'
 EMAIL_PORT = 587
 
 #Перенаправление после регистрации
